341bca01ff85eae2e2b7ab6e0a26b64b9183fa65
yurim <yurim2222@gmail.com>
2021-03-02 00:52:00 +0900

    recommand 완성해야함 board틀 잡아놓은상태, 메인+시계+슬라이드까지 완성

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,11 @@
 const deg = 6;
-const khr = document.querySelector("#khr");
-const kmn = document.querySelector("#kmn");
-const ksc = document.querySelector("#ksc");
-const uhr = document.querySelector("#uhr");
-const umn = document.querySelector("#umn");
-const usc = document.querySelector("#usc");
-const ohr = document.querySelector("#ohr");
-const omn = document.querySelector("#omn");
-const osc = document.querySelector("#osc");
+
 
 function kTime(params) {
+    const khr = document.querySelector("#khr");
+    const kmn = document.querySelector("#kmn");
+    const ksc = document.querySelector("#ksc");
+    
     let kday = new Date();
     let month = kday.getMonth() +1;
     let date = kday.getDate();
@@ -17,16 +13,29 @@ function kTime(params) {
     let week = ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'];
 
     let hh = kday.getHours();
+    let hh02 = kday.getHours() * 30;
     let mm = kday.getMinutes() ;
     let ss = kday.getSeconds();
     
 
-    khr.style.transform = `rotateZ(${hh  * 30 + mm / 12}deg)`;
+    khr.style.transform = `rotateZ(${hh02 + mm / 12 * deg}deg)`;
     kmn.style.transform = `rotateZ(${mm * deg}deg)`;
     ksc.style.transform = `rotateZ(${ss * deg}deg)`; 
 
+    
+    const kAnalog = document.getElementById('kAnalog');
+    if(hh > 6 && hh < 18 ){
+        kAnalog.style.background = ' url(../images/pc/main/main_clock_day.png)';
+        kAnalog.style.backgroundSize = 'cover';
+    }else {
+        kAnalog.style.background = ' url(../images/pc/main/main_clock_night.png)';
+        kAnalog.style.backgroundSize = 'cover';
+    }
+
     let dateObject = document.getElementById('kDate');
     let timeObject = document.getElementById('kTime');
+
+    
 
     if(hh < 10){
         hh = '0' + hh;
@@ -34,6 +43,8 @@ function kTime(params) {
     if(mm < 10){
         mm = '0' + mm;
     }
+
+    
 
     dateObject.textContent = `${month}월 ${date}일 ${week[day]}`;
     timeObject.textContent = `${hh} : ${mm}`
@@ -44,6 +55,9 @@ kTime();
 setInterval(kTime);
 
 setInterval(() => {
+    const uhr = document.querySelector("#uhr");
+    const umn = document.querySelector("#umn");
+    const usc = document.querySelector("#usc");
     let uday = new Date();
     let year = uday.getFullYear();
     let month = uday.getMonth() +1;
@@ -52,12 +66,13 @@ setInterval(() => {
     let week = ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'];
 
     let hh = (uday.getHours() - 9);
+    let hh02 = hh * 30;
     let mm = uday.getMinutes();
     let ss = uday.getSeconds();
 
     
 
-    uhr.style.transform = `rotateZ(${hh * 30 + mm / 12}deg)`;
+    uhr.style.transform = `rotateZ(${hh02 + mm / 12 * deg}deg)`;
     umn.style.transform = `rotateZ(${mm * deg}deg)`;
     usc.style.transform = `rotateZ(${ss * deg}deg)`; 
 
@@ -91,6 +106,19 @@ setInterval(() => {
             day = day -1;
         }
     }
+
+    
+    const uAnalog = document.querySelector('#uAnalog');
+
+    if(hh >= 6 || hh < 18 ){
+        
+        uAnalog.style.background = ' url(../images/pc/main/main_clock_day.png)';
+        uAnalog.style.backgroundSize = 'cover';
+    }else{
+        uAnalog.style.background = ' url(../images/pc/main/main_clock_night.png)';
+        uAnalog.style.backgroundSize = 'cover';
+    }
+
     if(hh < 10){
         hh = '0' + hh;
     }
@@ -105,6 +133,11 @@ setInterval(() => {
 });
 
 setInterval(() => {
+
+    const ohr = document.querySelector("#ohr");
+    const omn = document.querySelector("#omn");
+    const osc = document.querySelector("#osc");
+
     let oday = new Date();
     let year = oday.getFullYear();
     let month = oday.getMonth() +1;
@@ -113,12 +146,13 @@ setInterval(() => {
     let week = ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'];
 
     let hh = (oday.getHours() - 8);
+    let hh02 = hh * 30;
     let mm = oday.getMinutes();
     let ss = oday.getSeconds();
 
     
 
-    ohr.style.transform = `rotateZ(${hh * 30 + mm / 12}deg)`;
+    ohr.style.transform = `rotateZ(${hh02 + mm / 12 * deg}deg)`;
     omn.style.transform = `rotateZ(${mm * deg}deg)`;
     osc.style.transform = `rotateZ(${ss * deg}deg)`; 
 
@@ -152,6 +186,19 @@ setInterval(() => {
             day = day -1;
         }
     }
+        
+        
+        const oAnalog = document.querySelector('#oAnalog');
+
+        if(hh >= 6 || hh < 18 ){
+        
+            oAnalog.style.background = ' url(../images/pc/main/main_clock_day.png)';
+            oAnalog.style.backgroundSize = 'cover';
+        }else{
+            oAnalog.style.background = ' url(../images/pc/main/main_clock_night.png)';
+            oAnalog.style.backgroundSize = 'cover';
+        }
+
     if(hh < 10){
         hh = '0' + hh;
     }
--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ $( function() {
 $( function() {
     var swiper = new Swiper('main .swiper-container', {
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: 'main .swiper-button-next',
+          prevEl: 'main .swiper-button-prev',
         },
         autoplay: {
             delay: 5000,
@@ -25,3 +25,56 @@ $( function() {
         loop: true,
       });
   } );
+  //프로모션
+  $(function(){
+    
+    var swiper = new Swiper('.steady .swiper-container', {
+      navigation: {
+        nextEl: '.steady .swiper-button-next',
+        prevEl: '.steady .swiper-button-prev',
+      },
+      slidesPerView: 1,
+      spaceBetween: 10,
+      breakpoints: {
+        640: {
+          slidesPerView: 1.5,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1280: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+      }
+    });
+  });
+  
+  //이벤트
+  $(function(){
+    
+    var swiper = new Swiper('.event .swiper-container', {
+      navigation: {
+        nextEl: '.event .swiper-button-next',
+        prevEl: '.event .swiper-button-prev',
+      },
+      slidesPerView: 1,
+      spaceBetween: 10,
+      breakpoints: {
+        640: {
+          slidesPerView: 1.5,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1280: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+      }
+    });
+  })
