bf4ea539b857266238407ce71e8f528b723eb27b
yurim <yurim2222@gmail.com>
2021-03-03 00:57:09 +0900

    반응형 수정, 게시판미리보기까지 (스르륵 움직이도록 처리해야함)

--- js/main.js
+++ js/main.js
@@ -33,11 +33,11 @@ $( function() {
         nextEl: '.steady .swiper-button-next',
         prevEl: '.steady .swiper-button-prev',
       },
-      slidesPerView: 1,
+      slidesPerView: 1.5,
       spaceBetween: 10,
       breakpoints: {
         640: {
-          slidesPerView: 1.5,
+          slidesPerView: 2,
           spaceBetween: 20,
         },
         768: {
@@ -60,11 +60,11 @@ $( function() {
         nextEl: '.event .swiper-button-next',
         prevEl: '.event .swiper-button-prev',
       },
-      slidesPerView: 1,
+      slidesPerView: 1.5,
       spaceBetween: 10,
       breakpoints: {
         640: {
-          slidesPerView: 1.5,
+          slidesPerView:2,
           spaceBetween: 20,
         },
         768: {
@@ -77,4 +77,138 @@ $( function() {
         },
       }
     });
-  })
+  });
+
+
+  $(document).ready(function(){
+    var list = '.notice .list_wrap'; 
+    var up = '.notice .up_btn'; 
+    var down = '.notice .down_btn'; 
+    
+    var time = 3000; 
+    var speed = 100; 
+    var h = $(list).find('li').outerHeight(); 
+    
+    $(list).find('li').last().prependTo(list);
+    
+    $(list).find('li:eq(1) a').attr('tabindex','0');
+    
+    var topMove = function(){
+        $(list).stop().animate({
+            top: '-=' + h
+        },speed,function(){
+            $(list).find('li').first().appendTo(list);
+            
+            $(list).css('top',-70);
+            
+            $(list).find('li a').attr('tabindex','-1'); 
+            $(list).find('li:eq(1) a').attr('tabindex','0'); 
+        });
+    }
+    
+    var downMove = function(){
+        $(list).stop().animate({
+            top: '+=' + h 
+        },speed,function(){
+            $(list).find('li').last().prependTo(list);
+            $(list).css('top',-70);
+            
+            $(list).find('li a').attr('tabindex','-1');
+            $(list).find('li:eq(1) a').attr('tabindex','0');
+        });
+    }
+    
+    
+    var listMove = setInterval(topMove,time);
+    
+    $(list).find('li a').on('mouseenter focus',function(){
+        clearInterval(listMove);
+    });
+    
+    $(list).find('li a').on('mouseleave blur',function(){
+        listMove = setInterval(topMove,time);
+    });
+    
+    $(up).find('a').click(function(e){
+        e.preventDefault();
+        
+        clearInterval(listMove);
+        topMove(); 
+    });
+    
+    $(down).find('a').click(function(e){
+        e.preventDefault(); 
+        clearInterval(listMove);
+        
+        downMove(); 
+    });
+    
+    
+});
+
+
+$(document).ready(function(){
+  var list = '.review .list_wrap'; 
+  var up = '.review .up_btn'; 
+  var down = '.review .down_btn'; 
+  
+  var time = 3000; 
+  var speed = 100; 
+  var h = $(list).find('li').outerHeight(); 
+  
+  $(list).find('li').last().prependTo(list);
+  
+  $(list).find('li:eq(1) a').attr('tabindex','0');
+  
+  var topMove = function(){
+      $(list).stop().animate({
+          top: '-=' + h
+      },speed,function(){
+          $(list).find('li').first().appendTo(list);
+          
+          $(list).css('top',-70);
+          
+          $(list).find('li a').attr('tabindex','-1'); 
+          $(list).find('li:eq(1) a').attr('tabindex','0'); 
+      });
+  }
+  
+  var downMove = function(){
+      $(list).stop().animate({
+          top: '+=' + h 
+      },speed,function(){
+          $(list).find('li').last().prependTo(list);
+          $(list).css('top',-70);
+          
+          $(list).find('li a').attr('tabindex','-1');
+          $(list).find('li:eq(1) a').attr('tabindex','0');
+      });
+  }
+  
+  
+  var listMove = setInterval(topMove,time);
+  
+  $(list).find('li a').on('mouseenter focus',function(){
+      clearInterval(listMove);
+  });
+  
+  $(list).find('li a').on('mouseleave blur',function(){
+      listMove = setInterval(topMove,time);
+  });
+  
+  $(up).find('a').click(function(e){
+      e.preventDefault();
+      
+      clearInterval(listMove);
+      topMove(); 
+  });
+  
+  $(down).find('a').click(function(e){
+      e.preventDefault(); 
+      clearInterval(listMove);
+      
+      downMove(); 
+  });
+  
+  
+});
